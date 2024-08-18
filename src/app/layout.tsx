c46b741b89409f0db1ec278fef5295fa3ab0242c
vexra <unique123wowros@gmail.com>
2024-08-18 02:35:55 -0400

    Bismillah

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import theme from '@/theme'
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter'
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider } from '@mui/material/styles'
+import * as React from 'react'
+
+export default function RootLayout(props: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            {props.children}
+          </ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
+    </html>
+  )
+}
