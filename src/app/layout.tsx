52a256582c04debeff52d5ed8792f98a17bb734b
vexra <112993677+vexra@users.noreply.github.com>
2024-08-29 20:53:24 -0400

    feat: Configure auth hooks and auth utils (#6)
    * fix: remove parameter URL when redirect for unauthorized users
    * feat: Configure auth hooks and auth utils

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,26 @@
+import { auth } from '@/auth'
 import theme from '@/theme'
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter'
 import CssBaseline from '@mui/material/CssBaseline'
 import { ThemeProvider } from '@mui/material/styles'
+import { SessionProvider } from 'next-auth/react'
 import * as React from 'react'
 
-export default function RootLayout(props: { children: React.ReactNode }) {
+export default async function RootLayout(props: { children: React.ReactNode }) {
+  const session = await auth()
+
   return (
-    <html lang="id">
-      <body>
-        <AppRouterCacheProvider>
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
-            {props.children}
-          </ThemeProvider>
-        </AppRouterCacheProvider>
-      </body>
-    </html>
+    <SessionProvider session={session}>
+      <html lang="id">
+        <body>
+          <AppRouterCacheProvider>
+            <ThemeProvider theme={theme}>
+              <CssBaseline />
+              {props.children}
+            </ThemeProvider>
+          </AppRouterCacheProvider>
+        </body>
+      </html>
+    </SessionProvider>
   )
 }
